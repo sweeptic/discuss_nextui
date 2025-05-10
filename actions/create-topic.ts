2daa1e90975d78e63b9eb71a00b4e602d2c12890
Attila Horvath <sweeptic@gmail.com>
2025-05-10 15:29:01 +0200

    33. Handling General Form Errors

--- actions/create-topic.ts
+++ actions/create-topic.ts
@@ -1,12 +1,13 @@
 'use server';
 
+import { auth } from '@/auth';
 import { z } from 'zod';
 
 const createTopicSchema = z.object({
   name: z
     .string()
     .min(3)
-    .regex(/^[a-z-]/, {
+    .regex(/[a-z-]/, {
       message: 'Must be lowercase letters or dashes without spaces',
     }),
   description: z.string().min(10),
@@ -16,19 +17,31 @@ interface CreateTopicFormState {
   errors: {
     name?: string[];
     description?: string[];
+    _form?: string[];
   };
 }
 
 export async function createTopic(formState: CreateTopicFormState, formData: FormData): Promise<CreateTopicFormState> {
+  console.log('createTopic runs');
+
   const result = createTopicSchema.safeParse({
     name: formData.get('name'),
     description: formData.get('description'),
   });
 
   if (!result.success) {
-    console.log(result.error.flatten().fieldErrors);
+    // console.log(result.error.flatten().fieldErrors);
     return {
       errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ['You must be signed in to do this.'],
+      },
     };
   }
 
--- components/topics/topic-create-form.tsx
+++ components/topics/topic-create-form.tsx
@@ -4,10 +4,21 @@ import { Button } from '@heroui/button';
 import { Input, Textarea } from '@heroui/input';
 import { Popover, PopoverContent, PopoverTrigger } from '@heroui/popover';
 import * as actions from '@/actions';
-import { useActionState } from 'react';
+import { useActionState, useEffect, useState } from 'react';
 
 export default function TopicCreateForm() {
   const [formState, action] = useActionState(actions.createTopic, { errors: {} });
+
+  const [formErrorState, SetFormErrorState] = useState(formState);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+  useEffect(() => {
+    console.log('formState change');
+    SetFormErrorState(formState);
+  }, [formState]);
+
+  console.log('formErrorState', formErrorState);
 
   return (
     <Popover placement="left">
@@ -23,8 +34,10 @@ export default function TopicCreateForm() {
               label="Name"
               labelPlacement="outside"
               placeholder="Name"
-              isInvalid={!!formState.errors.name}
+              isInvalid={!!formErrorState.errors.name}
               errorMessage={formState.errors.name?.join(', ')}
+              onChange={(ev) => setName(ev.target.value)}
+              value={name}
             />
             {/* <div className="bg-red-400">{formState.errors.name?.join(', ')}</div> */}
             <Textarea
@@ -32,10 +45,18 @@ export default function TopicCreateForm() {
               label="Description"
               labelPlacement="outside"
               placeholder="Describe your topic"
-              isInvalid={!!formState.errors.description}
+              isInvalid={!!formErrorState.errors.description}
               errorMessage={formState.errors.description?.join(', ')}
+              onChange={(ev) => setDescription(ev.target.value)}
+              value={description}
             />
-            <Button type="submit">Submit</Button>
+            {formState.errors._form ? (
+              <div className="rounded p-2 bg-red-200 border border-red-400">{formState.errors._form?.join(', ')}</div>
+            ) : null}
+
+            <Button type="submit" onClick={() => SetFormErrorState({ errors: {} })}>
+              Submit
+            </Button>
           </div>
         </form>
       </PopoverContent>
