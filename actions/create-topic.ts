cc7d21b84bb9269fdc623160534dddadd5a4522d
Attila Horvath <sweeptic@gmail.com>
2025-05-10 13:41:44 +0200

    26. Adding Form Validation with Zod

--- actions/create-topic.ts
+++ actions/create-topic.ts
@@ -1,8 +1,24 @@
 'use server';
 
-export async function createTopic(formData: FormData) {
-  const name = formData.get('name');
-  const description = formData.get('description');
+import { z } from 'zod';
 
-  console.log('', name, description);
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: 'Must be lowercase letters or dashes without spaces',
+    }),
+  description: z.string().min(10),
+});
+
+export async function createTopic(formData: FormData) {
+  const result = createTopicSchema.safeParse({
+    name: formData.get('name'),
+    description: formData.get('description'),
+  });
+
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors);
+  }
 }
