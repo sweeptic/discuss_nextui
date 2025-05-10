de41269c3f5043450ede4db6ba3483cb0119072d
Attila Horvath <sweeptic@gmail.com>
2025-05-10 13:06:50 +0200

    22. Static Caching While Using Auth

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,51 +1,12 @@
 import { Navbar as HeroUINavbar, NavbarContent, NavbarItem } from '@heroui/navbar';
-import { Avatar } from '@heroui/avatar';
 
 import { ThemeSwitch } from '@/components/theme-switch';
-import { auth } from '@/auth';
 import Link from 'next/link';
 import { Input } from '@heroui/input';
-import { Button } from '@heroui/button';
 
-import * as actions from '@/actions';
-import { Popover, PopoverContent, PopoverTrigger } from '@heroui/popover';
+import HeaderAuth from './header-auth';
 
-export const Navbar = async () => {
-  const session = await auth();
-  let authContent: React.ReactNode;
-
-  if (session?.user) {
-    authContent = (
-      <Popover placement="left">
-        <PopoverTrigger>
-          <Avatar src={session.user.image || ''} />
-        </PopoverTrigger>
-        <PopoverContent>
-          <div className="p-4">
-            <form action={actions.signOut}>
-              <Button type="submit">Sign Out</Button>
-            </form>
-          </div>
-        </PopoverContent>
-      </Popover>
-    );
-  } else {
-    authContent = (
-      <>
-        <NavbarItem className="flex">
-          <form action={actions.signIn}>
-            <Button type="submit">Sign In</Button>
-          </form>
-        </NavbarItem>
-        <NavbarItem>
-          <form action={actions.signOut}>
-            <Button type="button">Sign Out</Button>
-          </form>
-        </NavbarItem>
-      </>
-    );
-  }
-
+export const Navbar = () => {
   return (
     <HeroUINavbar maxWidth="xl" position="sticky" className="bg-gray-200 mb-4">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
@@ -59,7 +20,7 @@ export const Navbar = async () => {
       </NavbarContent>
 
       <NavbarContent className=" sm:flex basis-1/5 sm:basis-full" justify="end">
-        {authContent}
+        <HeaderAuth />
         <NavbarItem className="hidden sm:flex gap-2">
           <ThemeSwitch />
         </NavbarItem>
