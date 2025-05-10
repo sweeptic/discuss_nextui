5bc2743858fd18a762ee123ac13834910cbc51dc
Attila Horvath <sweeptic@gmail.com>
2025-05-10 15:45:26 +0200

    34. Handling Database Errors in Forms

--- actions/create-topic.ts
+++ actions/create-topic.ts
@@ -1,6 +1,11 @@
 'use server';
 
 import { auth } from '@/auth';
+import { db } from '@/db';
+import paths from '@/paths';
+import { Topic } from '@prisma/client';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 import { z } from 'zod';
 
 const createTopicSchema = z.object({
@@ -30,7 +35,6 @@ export async function createTopic(formState: CreateTopicFormState, formData: For
   });
 
   if (!result.success) {
-    // console.log(result.error.flatten().fieldErrors);
     return {
       errors: result.error.flatten().fieldErrors,
     };
@@ -45,5 +49,32 @@ export async function createTopic(formState: CreateTopicFormState, formData: For
     };
   }
 
-  return { errors: {} };
+  let topic: Topic;
+
+  try {
+    // throw new Error('failed.');
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ['Something went wrong'],
+        },
+      };
+    }
+  }
+
+  revalidatePath('/');
+  redirect(paths.topicShow(topic.slug));
 }
