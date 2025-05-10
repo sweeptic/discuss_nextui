b4156914ba8d6bf03e9f6ad3f88105fdb801952d
Attila Horvath <sweeptic@gmail.com>
2025-05-10 14:19:53 +0200

    32. Displaying Validation Errors

--- components/topics/topic-create-form.tsx
+++ components/topics/topic-create-form.tsx
@@ -1,11 +1,13 @@
+'use client';
+
 import { Button } from '@heroui/button';
 import { Input, Textarea } from '@heroui/input';
 import { Popover, PopoverContent, PopoverTrigger } from '@heroui/popover';
 import * as actions from '@/actions';
-import { useFormState } from 'react-dom';
+import { useActionState } from 'react';
 
 export default function TopicCreateForm() {
-  const [formState, action] = useFormState(actions.createTopic, { errors: {} });
+  const [formState, action] = useActionState(actions.createTopic, { errors: {} });
 
   return (
     <Popover placement="left">
@@ -16,12 +18,22 @@ export default function TopicCreateForm() {
         <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80 ">
             <h3 className="text-lg">Create Topic</h3>
-            <Input name="name" label="Name" labelPlacement="outside" placeholder="Name" />
+            <Input
+              name="name"
+              label="Name"
+              labelPlacement="outside"
+              placeholder="Name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(', ')}
+            />
+            {/* <div className="bg-red-400">{formState.errors.name?.join(', ')}</div> */}
             <Textarea
               name="description"
               label="Description"
               labelPlacement="outside"
               placeholder="Describe your topic"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(', ')}
             />
             <Button type="submit">Submit</Button>
           </div>
