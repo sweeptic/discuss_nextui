569af3c822a8524daf1e2c99ecf83d9531547aba
Attila Horvath <sweeptic@gmail.com>
2025-05-12 11:01:29 +0200

    18. Alternative Type Names and Query Definitions

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -1,13 +1,14 @@
 import { db } from '@/db';
-import type { Post } from '@prisma/client';
 
-export type PostWithData = Post & {
-  topic: { slug: string };
-  user: { name: string | null };
-  _count: { comments: number };
-};
+// export type PostWithData = Post & {
+//   topic: { slug: string };
+//   user: { name: string | null };
+//   _count: { comments: number };
+// };
 
-export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
+export type PostWithData = Awaited<ReturnType<typeof fetchPostsByTopicSlug>>[number];
+
+export function fetchPostsByTopicSlug(slug: string) {
   return db.post.findMany({
     where: { topic: { slug: slug } },
     include: {
