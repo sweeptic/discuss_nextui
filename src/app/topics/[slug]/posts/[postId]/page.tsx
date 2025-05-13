2bb4cade4f2738a7f23408518c282f2eba725886
Attila Horvath <sweeptic@gmail.com>
2025-05-13 13:57:17 +0200

    02. Introducing Duplicate Queries

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import PostShow from '@/components/posts/post-show';
 import paths from '@/paths';
 import CommentCreateForm from '@/components/comments/comment-create-form';
+import CommentList from '@/components/comments/comment-list';
+import { fetchCommentsByPostId } from '@/db/queries/comments';
 
 interface PostShowPageProps {
   params: {
@@ -13,6 +15,8 @@ interface PostShowPageProps {
 export default async function PostShowPage({ params }: PostShowPageProps) {
   const { slug, postId } = params;
 
+  const comments = fetchCommentsByPostId;
+
   return (
     <div className="space-y-3">
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
@@ -20,7 +24,8 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
       </Link>
       <PostShow postId={postId} />
       <CommentCreateForm postId={postId} startOpen />
-      {/* <CommentList comments={comments} /> */}
+      {/* <CommentList fetchData={() => fetchCommentsByPostId(postId)} /> */}
+      <CommentList postId={postId} />
     </div>
   );
 }
--- src/components/comments/comment-list.tsx
+++ src/components/comments/comment-list.tsx
@@ -1,20 +1,18 @@
-import CommentShow from "@/components/comments/comment-show";
+import CommentShow from '@/components/comments/comment-show';
+import { fetchCommentsByPostId } from '@/db/queries/comments';
 
-interface CommentListProps {}
+interface CommentListProps {
+  postId: string;
+}
 
 // TODO: Get a list of comments from somewhere
-export default function CommentList({}: CommentListProps) {
-  const topLevelComments = comments.filter(
-    (comment) => comment.parentId === null
-  );
+export default async function CommentList({ postId }: CommentListProps) {
+  const comments = await fetchCommentsByPostId(postId);
+  //   const comments = await fetchData();
+
+  const topLevelComments = comments.filter((comment) => comment.parentId === null);
   const renderedComments = topLevelComments.map((comment) => {
-    return (
-      <CommentShow
-        key={comment.id}
-        commentId={comment.id}
-        comments={comments}
-      />
-    );
+    return <CommentShow key={comment.id} commentId={comment.id} postId={postId} />;
   });
 
   return (
