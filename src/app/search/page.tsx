250800bf5d59be3337f2b1099a8dfc39f2f99f77
Attila Horvath <sweeptic@gmail.com>
2025-05-14 15:28:25 +0200

    11. Running the Search

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,3 +1,5 @@
+import PostList from '@/components/posts/post-list';
+import { fetchPostsBySearchTerm } from '@/db/queries/posts';
 import { redirect } from 'next/navigation';
 
 interface SearchPageProps {
@@ -13,5 +15,9 @@ export default async function SearchPage({ searchParams }: SearchPageProps) {
     redirect('/');
   }
 
-  return <div>{term}</div>;
+  return (
+    <div>
+      <PostList fetchData={() => fetchPostsBySearchTerm(term)} />
+    </div>
+  );
 }
