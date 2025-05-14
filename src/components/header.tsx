c959ac81ef9e00ca696477165631b86c779045a3
Attila Horvath <sweeptic@gmail.com>
2025-05-14 15:34:36 +0200

    12. The De-Opt to Client Side Rendering Warning

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/react';
 import HeaderAuth from '@/components/header-auth';
 import SearchInput from './search-input';
+import { Suspense } from 'react';
 
 export default function Header() {
   return (
@@ -13,7 +14,9 @@ export default function Header() {
       </NavbarBrand>
       <NavbarContent justify="center">
         <NavbarItem>
-          <SearchInput />
+          <Suspense>
+            <SearchInput />
+          </Suspense>
         </NavbarItem>
       </NavbarContent>
 
