a705ed75dd79898cbde888b2b1c698d23afb4ad8
Attila Horvath <sweeptic@gmail.com>
2025-05-14 14:53:01 +0200

    07. Accessing the Query String

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,5 +1,9 @@
+'use client';
 import { Input } from '@nextui-org/react';
+import { useSearchParams } from 'next/navigation';
 
 export default function SearchInput() {
-  return <Input />;
+  const searchParams = useSearchParams();
+
+  return <Input defaultValue={searchParams.get('term') || ''} />;
 }
